8f1d4310f75e14755c4aa5681a70a01113fb4df1
Matheus Gomes <32970550+MatheusHoudin@users.noreply.github.com>
2021-04-02 16:32:34 -0300

    Revert "Feature/import products page"

--- lib/core/extension/time_of_day_extensions.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-extension TimeOfDayExtension on TimeOfDay {
-  String convertTimeOfDayToHourMinuteString() {
-    final hour = this.hour > 9 ? this.hour : "0${this.hour}";
-    final minute = this.minute > 9 ? this.minute : "0${this.minute}";
-    return "$hour:$minute";
-  }
-}
--- lib/shared/model/beep_inventory_status.dart
+++
@@ -1,5 +0,0 @@
-enum BeepInventoryStatus {
-  NotStarted,
-  Started,
-  Finished
-}
