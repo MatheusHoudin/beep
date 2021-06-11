7b686ba9160d1b1ca4b453b60edc0a5a803356fe
Matheus Gomes <matheusdin04@gmail.com>
2021-06-10 21:08:23 -0300

    personalizing allocation header

--- lib/features/employeeinventoryallocations/presentation/pages/employee_inventory_allocations_page.dart
+++ lib/features/employeeinventoryallocations/presentation/pages/employee_inventory_allocations_page.dart
@@ -2,12 +2,27 @@ import 'package:beep/core/constants/assets.dart';
 import 'package:beep/core/constants/colors.dart';
 import 'package:beep/core/constants/dimens.dart';
 import 'package:beep/core/constants/texts.dart';
+import 'package:beep/shared/model/beep_inventory.dart';
 import 'package:beep/shared/widgets/app_bar_details_section.dart';
 import 'package:beep/shared/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class EmployeeInventoryAllocationsPage extends StatelessWidget {
+class EmployeeInventoryAllocationsPage extends StatefulWidget {
+  @override
+  _EmployeeInventoryAllocationsPageState createState() => _EmployeeInventoryAllocationsPageState();
+}
+
+class _EmployeeInventoryAllocationsPageState extends State<EmployeeInventoryAllocationsPage> {
+  BeepInventory _inventory;
+
+  @override
+  void initState() {
+    super.initState();
+    _inventory = Get.arguments as BeepInventory;
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -17,8 +32,8 @@ class EmployeeInventoryAllocationsPage extends StatelessWidget {
             CustomAppBar(
               isWhiteStyle: true,
               hasIcon: true,
-              icon: inventoryDetailsProductsIcon,
-              appBarTitle: "nome do inventario",
+              icon: inventoryItemIcon,
+              appBarTitle: _inventory.name,
               onBackPressed: () => null,
             ),
             AppBarDetailsSection(
@@ -26,10 +41,7 @@ class EmployeeInventoryAllocationsPage extends StatelessWidget {
               bottomSection: Text(
                 employeeInventoryAllocationsInstructions,
                 textAlign: TextAlign.center,
-                style: GoogleFonts.firaSans(
-                  fontSize: smallTextSize,
-                  color: grayColor
-                ),
+                style: GoogleFonts.firaSans(fontSize: smallTextSize, color: grayColor),
               ),
             ),
             Expanded(
