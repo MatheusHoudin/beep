2930c461bff7361e36283866ad68032e88c8e7da
Matheus Gomes <matheusdin04@gmail.com>
2021-06-14 22:31:24 -0300

    configuring initial camera behavior

--- lib/features/registercounting/presentation/pages/register_counting_page.dart
+++ lib/features/registercounting/presentation/pages/register_counting_page.dart
@@ -2,9 +2,11 @@ import 'package:beep/core/constants/assets.dart';
 import 'package:beep/core/constants/colors.dart';
 import 'package:beep/core/constants/dimens.dart';
 import 'package:beep/features/registercounting/domain/controller/register_counting_controller.dart';
+import 'package:beep/features/registercounting/presentation/widgets/counting_action_toggle.dart';
 import 'package:beep/shared/model/inventory_counting_allocation.dart';
 import 'package:beep/shared/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_qr_bar_scanner/qr_bar_scanner_camera.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -27,6 +29,7 @@ class _RegisterCountingPageState extends State<RegisterCountingPage> {
         backgroundColor: secondaryColor,
         body: GetBuilder<RegisterCountingController>(
           builder: (c) => Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               CustomAppBar(
                 isWhiteStyle: false,
@@ -37,7 +40,7 @@ class _RegisterCountingPageState extends State<RegisterCountingPage> {
                 onBackPressed: () => null,
               ),
               LocationText(c.getLocationName()),
-              Expanded(child: Container())
+              Expanded(child: ContentSection())
             ],
           ),
         ),
@@ -48,7 +51,67 @@ class _RegisterCountingPageState extends State<RegisterCountingPage> {
   Widget LocationText(String location) {
     return Text(
       location,
+      textAlign: TextAlign.center,
       style: GoogleFonts.firaSans(color: countingGray, fontSize: normalTextSize),
+    );
+  }
+
+  Widget ContentSection() {
+    return Padding(
+      padding: EdgeInsets.only(top: mediumSize, left: normalSize, right: normalSize),
+      child: Column(
+        children: [
+          CountingActionsSection(),
+          BarcodeCameraSection()
+        ],
+      ),
+    );
+  }
+
+  Widget CountingActionsSection() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        CameraToggleAction(),
+        SizedBox(
+          width: smallSize,
+        ),
+        FlashlightToggleAction()
+      ],
+    );
+  }
+
+  Widget CameraToggleAction() {
+    return CountingActionToggle(
+      color: primaryColor,
+      icon: Icon(
+        Icons.visibility_outlined,
+        color: Colors.white,
+      ),
+      onClick: () => null,
+    );
+  }
+
+  Widget FlashlightToggleAction() {
+    return CountingActionToggle(
+      color: secondaryNegativeColor,
+      icon: Icon(
+        Icons.flash_off,
+        color: Colors.white,
+      ),
+      onClick: () => null,
+    );
+  }
+
+  Widget BarcodeCameraSection() {
+    return Container(
+      width: Get.size.width,
+      height: Get.size.height * 0.4,
+      child: QRBarScannerCamera(
+        qrCodeCallback: (barcode) {
+          print(barcode);
+        },
+      ),
     );
   }
 }
